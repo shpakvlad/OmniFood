418d24760e60d6157022c1dfba75bd2a8f44808a
[spakvlad] <[vladyslav.shpakov@gmail.com]>
2023-12-21 22:55:13 +0200

    added create dynamic header & features

---
+++ res/data/data.js
@@ -0,0 +1,107 @@
+/************************************************/
+/*                    EXPORT                    */
+/************************************************/
+export const data = [
+    {
+        name: "header",
+        logo: {
+            link: "./res/img/logo/logo-white.png",
+            class: "logo",
+            alt: "Omnifood logo",
+        },
+
+        nav: [
+            {
+                text: "Food delivery",
+                link: "#",
+            },
+            {
+                text: "How it works",
+                link: "#",
+            },
+            {
+                text: "Our cities",
+                link: "#",
+            },
+            {
+                text: "Sign up",
+                link: "#",
+                id: "sign-up__btn",
+            },
+            {
+                text: "Log in",
+                link: "#",
+                id: "log-in__btn",
+            },
+        ],
+
+        hero: {
+            class: "hero-text-box",
+            text: "Goodbye junk food.<br />Hello super healthy meals.",
+            buttons: [
+                {
+                    text: "I’m hungry",
+                    link: "#",
+                    class: "btn btn-full",
+                },
+                {
+                    text: "Show me more",
+                    link: "#",
+                    class: "btn btn-ghost",
+                },
+            ],
+        },
+    },
+
+    {
+        name: "features",
+        h2: "Get food fast &mdash; not fast food.",
+        p: {
+            class: "long-text",
+            text: "Hello, we’re Omnifood, your new premium food delivery service. We know you’re always busy. No time for cooking. So let us take care of that, we’re really good at it, we promise!",
+        },
+
+        boxes: [
+            {
+                class: "box",
+                ionicon: {
+                    class: "icon-big",
+                    name: "infinite",
+                },
+
+                h3: "Up to 365 days/year",
+                p: "Never cook again! We really mean that. Our subscription plans include up to 365 days/year coverage. You can also choose to order more flexibly if that’s your style.",
+            },
+            {
+                class: "box",
+                ionicon: {
+                    class: "icon-big",
+                    name: "stopwatch",
+                },
+
+                h3: "Ready in 20 minutes",
+                p: "You're only twenty minutes away from your delicious and super healthy meals delivered right to your home. We work with the best chefs in each town to ensure that you're 100% happy.",
+            },
+            {
+                class: "box",
+                ionicon: {
+                    class: "icon-big",
+                    name: "leaf",
+                },
+
+                h3: "100% organic",
+                p: "Nll our vegetables are fresh, organic and local. Animals are raised without added hormones or antibiotics. Good for your health, the environment, and it also tastes better!",
+            },
+            {
+                class: "box",
+                ionicon: {
+                    class: "icon-big",
+                    name: "card",
+                },
+
+                h3: "Order anything",
+                p: "We don't limit your creativity, which means you can order whatever you feel like. You can also choose from our menu containing over 100 delicious meals. It's up to you!",
+            },
+        ],
+    },
+];
--- res/js/main.js
+++ res/js/main.js
@@ -1,3 +1,5 @@
+import { data } from "../data/data.js";
+
 /************************************************/
 /*                    SWIPER                    */
 /************************************************/
@@ -11,7 +13,102 @@ const swiper = new Swiper(".swiper", {
 });
 
 /************************************************/
-/*                   CLASSES                    */
+/*                     MVC                      */
 /************************************************/
-const signUp = new UsersController(new UsersModel, new UsersView);
+const signUp = new UsersController(new UsersModel(), new UsersView());
+setData();
 signUp.init();
+
+/************************************************/
+/*                     DATA                     */
+/************************************************/
+function setData() {
+    data.forEach((element) => {
+        if (element.name == "header") {
+            /* ===== HEADER ===== */
+            let header = document.querySelector("header .container");
+
+            //logo
+            const img = document.createElement("img");
+
+            img.className = element.logo.class;
+            img.setAttribute("src", element.logo.link);
+            img.setAttribute("alt", element.logo.alt);
+            header.append(img);
+
+            //nav
+            const nav = document.createElement("nav");
+            element.nav.forEach((element) => {
+                const a = document.createElement("a");
+                a.setAttribute("href", element.link);
+                a.textContent = element.text;
+                if (element.hasOwnProperty("id")) {
+                    a.id = element.id;
+                }
+
+                nav.appendChild(a);
+            });
+            header.append(nav);
+
+            //hero
+            const hero = document.createElement("div");
+            hero.className = element.hero.class;
+
+            const h1 = document.createElement("h1");
+            h1.innerHTML = element.hero.text;
+            hero.appendChild(h1);
+
+            element.hero.buttons.forEach((element) => {
+                const a = document.createElement("a");
+                a.textContent = element.text;
+                a.setAttribute("href", element.link);
+                a.className = element.class;
+
+                hero.appendChild(a);
+            });
+            header.append(hero);
+        }
+
+        /* ===== FEATURES ===== */
+        if (element.name == "features") {
+            let features = document.querySelector(
+                ".section-features .container"
+            );
+
+            const h2 = document.createElement("h2");
+            h2.innerHTML = element.h2;
+
+            const p = document.createElement("p");
+            p.className = element.p.class;
+            p.innerHTML = element.p.text;
+
+            features.append(h2);
+            features.append(p);
+
+            /* ===== CARDS ===== */
+            const featuresBoxes = document.querySelector(
+                ".section-features .container:nth-child(2)"
+            );
+
+            element.boxes.forEach((element) => {
+                const div = document.createElement("div");
+                div.className = element.class;
+
+                const ion = document.createElement("ion-icon");
+                ion.className = element.ionicon.class;
+                ion.setAttribute("name", element.ionicon.name);
+                div.appendChild(ion);
+
+                const h3 = document.createElement("h3");
+                h3.textContent = element.h3;
+                div.appendChild(h3);
+
+                const p = document.createElement("p");
+                p.textContent = element.p;
+                div.appendChild(p);
+
+                featuresBoxes.append(div);
+            });
+        }
+    });
+}
